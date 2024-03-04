4cde91ba6bb35d0fe4f261215270173e05729b2e
Himanshu <himanshubhushan775@gmail.com>
2024-03-05 00:25:39 +0530

    removed unused dep

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { verify, sign, decode } from 'hono/jwt'
-import { TraceException } from '@cloudflare/workers-types'
 import { userRouter } from './routes/user'
 import { blogRouter } from './routes/blog'
 
